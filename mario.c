9d9fdd82a2464772fcf9088786870269601cc913
alighazi96 <alighazi96@users.noreply.github.com>
2020-09-14 00:23:49 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -7,22 +7,23 @@ int main(void)
     int size;
     int i;
     int j;
-  do
-  {
-      size=get_int("enter the size: ");
-  }while(1>size||size>8);
-    for(i=0;i<size;i++)
-  {
-        for(j=0;j<size-i-1;j++)
-     {
-        printf(" ");
-     }
-        for(j=0;j<i+1;j++)
-     {
-        printf("#");
-     }
-     printf("\n");
-  }
+    do
+    {
+        size = get_int("enter the size: ");
+    }
+    while (1 > size || size > 8);
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 
 
 }
