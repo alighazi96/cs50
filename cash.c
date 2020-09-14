74877510fc9038eec4d8646aa416393dd79a3385
alighazi96 <alighazi96@users.noreply.github.com>
2020-09-14 01:10:10 +0000

    automated commit by submit50

--- cash.c
+++ cash.c
@@ -8,34 +8,34 @@ int main(void)
     
     do
     {
-        dollars=get_float("change owed: ");
+        dollars = get_float("change owed: ");
         
-    }while(dollars<0);
+    }
+    while (dollars < 0);
+    int cents = round(dollars * 100);
+    int coins = 0;
   
-  int cents=round(dollars*100);
-  int coins=0;
-  
-  while(cents>=25)
-  {
-      cents-=25;
-      coins++;
-  }
-  while(cents>=10)
-  {
-      cents-=10;
-      coins++;
-  }
-  while(cents>=5)
-  {
-      cents-=5;
-      coins++;
-  }
-  while(cents>=1)
-  {
-      cents-=1;
-      coins++;
-  }
-  printf("no of coins=%i\n",coins);
+    while (cents >= 25)
+    {
+        cents -= 25;
+        coins++;
+    }
+    while (cents >= 10)
+    {
+        cents -= 10;
+        coins++;
+    }
+    while (cents >= 5)
+    {
+        cents -= 5;
+        coins++;
+    }
+    while (cents >= 1)
+    {
+        cents -= 1;
+        coins++;
+    }
+    printf("no of coins=%i\n", coins);
   
   
     
