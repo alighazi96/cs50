01116951aaa3d0ba435fbe597a3f1e7d4559c02d
alighazi96 <alighazi96@users.noreply.github.com>
2020-09-14 23:55:05 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,40 +1,38 @@
 #include<stdio.h>
-#include<cs50.h>
 #include<ctype.h>
-#include<stdlib.h>
+#include<cs50.h>
 #include<string.h>
+#include<stdlib.h>
 
 int main(int argc,string argv[])
 {
-    if(argc != 2)
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
+    if(argc != 2 )
+    {printf("Usage: ./caesar key\n");
+    return 1;
     }
-
-    int key =atoi(argv[1]);
-
-    string plaintext = get_string("plaintext:");
-
-    printf("ciphertext: ");
-
-    for(int i = 0,n = strlen(plaintext);i < n;i++)
+    
+    int key = atoi(argv[1]);
+    
+    string plaintext = get_string("plaintext: ");
+    
+    printf("chipertext: ");
+    for(int i=0,n = strlen(plaintext);i < n;i++)
     {
-        char c = plaintext[i];
+       char c=plaintext[i];
+    
         if(isalpha(c))
         {
-            char m = 'A';
+            char m ='A';
             if(islower(c))
-            {
                 m='a';
-                printf("%c",(c - m + key) % 26 +m);
-            }
-            else
-            {
-            printf("%c\n",c);
-            }
+            printf("%c\n",(c - m + key)%26 + m);    
+        }
+        else
+        {
+            printf("%c",c);
         }
     }
+    
 
-
-}
+} 
+    
