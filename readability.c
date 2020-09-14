88ca49395668d03da171c74e62e612b73cdbf172
alighazi96 <alighazi96@users.noreply.github.com>
2020-09-14 00:15:50 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -9,49 +9,48 @@ int main(void)
     
     printf("text:");
     float formula;
-    int letter=0;
-    int word=0;
-    int sentence=0;
-    string s=get_string("");
+    int letter = 0;
+    int word = 0;
+    int sentence = 0;
+    string s = get_string("");
     
  
     
-    for(int i=0,n=strlen(s);i<n;i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
         
-       if(isalpha(s[i]))  
-       {
-       letter++;
-       }
+        if (isalpha(s[i]))  
+        {
+            letter++;
+        }
        
     }
-    
-    for(int i=0,n=strlen(s);i<n;i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(isblank(s[i]))
-       {
-           word++;
-       }
+        if (isblank(s[i]))
+        {
+            word++;
+        }
     }
-    for(int i=0,n=strlen(s);i<n;i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(s[i]=='.'||s[i]=='?'||s[i]=='!')
+        if (s[i] == '.' || s[i] == '?' || s[i] == '!')
         {
             sentence++;
         }
     }
     word++;
     
-    formula=(0.0588 * (letter*100/word)) - (0.296 * (sentence*100/word)) - 15.8;
-    if(1<=formula&&formula<16)
+    formula = (0.0588 * (letter * 100 / word)) - (0.296 * (sentence * 100 / word)) - 15.8;
+    if (1 <= formula && formula < 16)
     {
-    printf("Grade %d\n",(int)round(formula));
+        printf("Grade %d\n", (int)round(formula));
     }
-    else if(formula>16)
+    else if (formula > 16)
     {
-    printf("Grade 16+\n");
+        printf("Grade 16+\n");
     }
-    else if(formula<1)
+    else if (formula < 1)
     {
         printf("Grade:Before Grade 1\n");
     }
